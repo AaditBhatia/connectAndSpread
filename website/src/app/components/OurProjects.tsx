9567787110148400b071ec1748a8a85ef7ecef57
Aadit <aadit.bhatia93@gmail.com>
2023-07-08 11:31:20 -0700

    Update OurProjects.tsx

--- website/src/app/components/OurProjects.tsx
+++ website/src/app/components/OurProjects.tsx
@@ -3,34 +3,36 @@ import DonatePic from './DonatePic';
 
 const OurProjects = () => {
   return (
-    <div className="flex justify-center">
-      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-1 sm:mx-4 md:mx-6 lg:mx-8">
-        <DonatePic />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Project 1</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
+    <div className="flex justify-center bg-purple-50">
+      <div className="flex flex-wrap items-start gap-4">
+        <div className="max-w-xs rounded overflow-hidden shadow-lg mb-12 mt-4">
+          <DonatePic />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Card 1</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 sm:mx-8 md:mx-12 lg:mx-16">
-        <DonatePic />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Project 2</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg mb-12 mt-4">
+          <DonatePic />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Card 2</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="max-w-xs rounded overflow-hidden shadow-lg mx-2 sm:mx-4 md:mx-6 lg:mx-8">
-        <DonatePic />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Project 3</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg mb-12 mt-4">
+          <DonatePic />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Card 3</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
         </div>
       </div>
     </div>
